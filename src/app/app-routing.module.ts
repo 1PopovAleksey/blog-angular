a131d83fe582a8e2354de79dcaa5860e86d93950
Aleksey Popov <alekseypopov.dev@gmail.com>
2023-02-15 11:14:35 +0300

    feat: update app

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { BlogComponent } from './blog/blog.component';
+import { CreateComponent } from './create/create.component';
+
+const routes: Routes = [
+  { path: 'blog', component: BlogComponent },
+  { path: 'create', component: CreateComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,8 +5,11 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import { BlogComponent } from './blog/blog.component';
+import { CreateComponent } from './create/create.component';
+
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, BlogComponent, CreateComponent],
   imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule],
   providers: [],
   bootstrap: [AppComponent],
